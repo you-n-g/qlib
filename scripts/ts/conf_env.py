6f8718c4c0a139fd4c12c3b652cd68392b3c14c6
Young <afe.young@gmail.com>
2025-11-11 01:33:14 +0000

    feat: add script to set DS_RUNNER_COSTEER_DUMP_STDOUT_TYPE in .env

---
+++ scripts/ts/conf_env.py
@@ -0,0 +1,7 @@
+# Use dotenv to set the below environment variables in the .env file
+# This will update or add the variables directly in the .env file inplace
+from dotenv import set_key
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent.parent.parent / '.env'
+set_key(str(env_path), 'DS_RUNNER_COSTEER_DUMP_STDOUT_TYPE', 'full')
